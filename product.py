8e991bdfe8d895d1ecba926095b3c8716f53f8ff
Alex <alexprengere@gmail.com>
2015-12-07 21:33:45 +0100

    Reduce comments

--- product.py
+++ product.py
@@ -19,23 +19,7 @@ the sum of the indices of the elements.
 (2, 1)
 (2, 2)
 
-Now let's try with sqrt as objective function to minimize.
-Note that (1, 1) is now after (0, 2) and (2, 0).
-
->>> from math import sqrt
->>> for t in product([[0, 1, 2], [0, 1, 2]], key=sqrt):
-...     print(t)
-(0, 0)
-(0, 1)
-(1, 0)
-(0, 2)
-(2, 0)
-(1, 1)
-(1, 2)
-(2, 1)
-(2, 2)
-
-Change that to the square.
+Now let's try with an objective function to minimize.
 Note that (1, 1) is now before (0, 2) and (2, 0).
 
 >>> for t in product([[0, 1, 2], [0, 1, 2]], key=lambda i: i**2):
